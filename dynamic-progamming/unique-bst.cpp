163d35b5b182e3bf93d98cfb554c4ef201c17f66
Mohit Sharma <mohitdtumce@gmail.com>
2023-09-05 14:40:50 +0530

    Create unique-bst.cpp

---
+++ dynamic-progamming/unique-bst.cpp
@@ -0,0 +1,12 @@
+int numTrees(int n) {
+   if (n <= 1)
+    return 1; 
+    vector<int> dp(n+1, 0);
+    dp[0] = dp[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        for (int j = 0; j < i; j++) {
+            dp[i] += (dp[j] * dp[i - j - 1]);
+        }
+    }
+    return dp[n];
+}
